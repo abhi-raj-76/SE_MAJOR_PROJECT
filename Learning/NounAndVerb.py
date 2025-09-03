071b32e23fb2469157ffaebbf226d8f0001e18d8
ABHISHEK KUMAR RAJ <24mcmc24@uohyd.ac.in>
2025-09-03 05:19:53 +0000

    adding text from some other file.txt then identify nouns and verbs

--- Learning/NounAndVerb.py
+++ Learning/NounAndVerb.py
@@ -2,11 +2,8 @@ import nltk
 
 from nltk.tokenize import word_tokenize
 
-text = """Renewable energy is an essential component of sustainable development.
-Unlike fossil fuels, which are finite and contribute to environmental degradation,
-renewable energy sources such as solar, wind, and hydroelectric power offer a clean and sustainable alternative. These energy sources are abundant and can significantly reduce greenhouse gas emissions, helping to combat climate change.
-By investing in renewable energy, 
-countries can create jobs, enhance energy security, and promote environmental sustainability."""
+with open('File.txt','r') as File:
+    text = File.read()
 
 tokenized_word = word_tokenize(text);
 #print(tokenized_word)
